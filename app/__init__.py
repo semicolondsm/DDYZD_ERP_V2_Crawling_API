f7939648e6e40e8897916e07b40bb596c7a9dff5
조호원 <hwc9169@gmail.com>
2021-01-12 21:53:27 +0900

    UPDATE; add jwt manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, get_jwt_identity
 from config import config
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
     db.init_app(app)
+    jwt.init_app(app)
+
+    from app.api import api
+    app.register_blueprint(api)
     return app
